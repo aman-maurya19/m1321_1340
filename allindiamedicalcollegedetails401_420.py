39c359fca5cbcde1f82db4ec10461bba5b8b7597
AMAN MAURYA <amanmaurya12yy@gmail.com>
2026-03-23 12:25:38 +0530

    Update allindiamedicalcollegedetails401_420.py

--- allindiamedicalcollegedetails401_420.py
+++ allindiamedicalcollegedetails401_420.py
@@ -9237,7 +9237,7 @@ def parse_faq_scholarships_section(driver, URLS):
 def scrape_mba_colleges():
     driver = create_driver()
     all_data = []
-    c_count = 1321
+    c_count = 1331
 
     try:
         for base_url in BASE_URL:
